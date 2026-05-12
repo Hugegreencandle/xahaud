beartec-jpg/FalconLedger

Remove directory size limit (#5935)
This change introduces the `fixDirectoryLimit` amendment to remove the directory pages limit. We found that the directory size limit is easier to hit than originally assumed, and there is no good reason to keep this limit, since the object reserve provides the necessary incentive to avoid creating unnecessary objects on the ledger.

include/xrpl/protocol/Protocol.h
@@ -55,7 +55,10 @@ std::size_t constexpr oversizeMetaDataCap = 5200;
 /** The maximum number of entries per directory page */
 std::size_t constexpr dirNodeMaxEntries = 32;
 
-/** The maximum number of pages allowed in a directory */
+/** The maximum number of pages allowed in a directory
+
+    Made obsolete by fixDirectoryLimit amendment.
+*/
 std::uint64_t constexpr dirNodeMaxPages = 262144;
 
 /** The maximum number of items in an NFT page */

src/test/app/Credentials_test.cpp
@@ -558,6 +558,39 @@ struct Credentials_test : public beast::unit_test::suite
                     jle[jss::result][jss::node]["CredentialType"] ==
                         strHex(std::string_view(credType)));
             }
+
+            {
+                testcase("Credentials fail, directory full");
+                std::uint32_t const issuerSeq{env.seq(issuer) + 1};
+                env(ticket::create(issuer, 63));
+                env.close();
+
+                // Everything below can only be tested on open ledger.
+                auto const res1 = directory::bumpLastPage(
+                    env,
+                    directory::maximumPageIndex(env),
+                    keylet::ownerDir(issuer.id()),
+                    directory::adjustOwnerNode);
+                BEAST_EXPECT(res1);
+
+                auto const jv = credentials::create(issuer, subject, credType);
+                env(jv, ter(tecDIR_FULL));
+                // Free one directory entry by using a ticket
+                env(noop(issuer), ticket::use(issuerSeq + 40));
+
+                // Fill subject directory
+                env(ticket::create(subject, 63));
+                auto const res2 = directory::bumpLastPage(
+                    env,
+                    directory::maximumPageIndex(env),
+                    keylet::ownerDir(subject.id()),
+                    directory::adjustOwnerNode);
+                BEAST_EXPECT(res2);
+                env(jv, ter(tecDIR_FULL));
+
+                // End test
+                env.close();
+            }
         }
 
         {
@@ -1084,6 +1117,7 @@ struct Credentials_test : public beast::unit_test::suite
         testSuccessful(all);
         testCredentialsDelete(all);
         testCreateFailed(all);
+        testCreateFailed(all - fixDirectoryLimit);
         testAcceptFailed(all);
         testDeleteFailed(all);
         testFeatureFailed(all - featureCredentials);

src/test/jtx/directory.h
@@ -1,0 +1,81 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2025 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_TEST_JTX_DIRECTORY_H_INCLUDED
+#define RIPPLE_TEST_JTX_DIRECTORY_H_INCLUDED
+
+#include <test/jtx/Env.h>
+
+#include <xrpl/basics/Expected.h>
+#include <xrpl/protocol/Feature.h>
+#include <xrpl/protocol/Indexes.h>
+
+#include <cstdint>
+#include <limits>
+
+namespace ripple::test::jtx {
+
+/** Directory operations. */
+namespace directory {
+
+enum Error {
+    DirectoryRootNotFound,
+    DirectoryTooSmall,
+    DirectoryPageDuplicate,
+    DirectoryPageNotFound,
+    InvalidLastPage,
+    AdjustmentError
+};
+
+/// Move the position of the last page in the user's directory on open ledger to
+/// newLastPage. Requirements:
+/// - directory must have at least two pages (root and one more)
+/// - adjust should be used to update owner nodes of the objects affected
+/// - newLastPage must be greater than index of the last page in the directory
+///
+/// Use this to test tecDIR_FULL errors in open ledger.
+/// NOTE: effects will be DISCARDED on env.close()
+auto
+bumpLastPage(
+    Env& env,
+    std::uint64_t newLastPage,
+    Keylet directory,
+    std::function<bool(ApplyView&, uint256, std::uint64_t)> adjust)
+    -> Expected<void, Error>;
+
+/// Implementation of adjust for the most common ledger entry, i.e. one where
+/// page index is stored in sfOwnerNode (and only there). Pass this function
+/// to bumpLastPage if the last page of directory has only objects
+/// of this kind (e.g. ticket, DID, offer, deposit preauth, MPToken etc.)
+bool
+adjustOwnerNode(ApplyView& view, uint256 key, std::uint64_t page);
+
+inline auto
+maximumPageIndex(Env const& env) -> std::uint64_t
+{
+    if (env.enabled(fixDirectoryLimit))
+        return std::numeric_limits<std::uint64_t>::max();
+    return dirNodeMaxPages - 1;
+}
+
+}  // namespace directory
+
+}  // namespace ripple::test::jtx
+
+#endif

src/test/ledger/Directory_test.cpp
@@ -22,9 +22,11 @@
 #include <xrpl/ledger/Sandbox.h>
 #include <xrpl/protocol/Feature.h>
 #include <xrpl/protocol/Protocol.h>
+#include <xrpl/protocol/TER.h>
 #include <xrpl/protocol/jss.h>
 
 #include <algorithm>
+#include <limits>
 
 namespace ripple {
 namespace test {
@@ -490,6 +492,91 @@ struct Directory_test : public beast::unit_test::suite
     }
 
     void
+    testDirectoryFull()
+    {
+        using namespace test::jtx;
+        Account alice("alice");
+
+        auto const testCase = [&, this](FeatureBitset features, auto setup) {
+            using namespace test::jtx;
+
+            Env env(*this, features);
+            env.fund(XRP(20000), alice);
+            env.close();
+
+            auto const [lastPage, full] = setup(env);
+
+            // Populate root page and last page
+            for (int i = 0; i < 63; ++i)
+                env(credentials::create(alice, alice, std::to_string(i)));
+            env.close();
+
+            // NOTE, everything below can only be tested on open ledger because
+            // there is no transaction type to express what bumpLastPage does.
+
+            // Bump position of last page from 1 to highest possible
+            auto const res = directory::bumpLastPage(
+                env,
+                lastPage,
+                keylet::ownerDir(alice.id()),
+                [lastPage, this](
+                    ApplyView& view, uint256 key, std::uint64_t page) {
+                    auto sle = view.peek({ltCREDENTIAL, key});
+                    if (!BEAST_EXPECT(sle))
+                        return false;
+
+                    BEAST_EXPECT(page == lastPage);
+                    sle->setFieldU64(sfIssuerNode, page);
+                    // sfSubjectNode is not set in self-issued credentials
+                    view.update(sle);
+                    return true;
+                });
+            BEAST_EXPECT(res);
+
+            // Create one more credential
+            env(credentials::create(alice, alice, std::to_string(63)));
+
+            // Not enough space for another object if full
+            auto const expected = full ? ter{tecDIR_FULL} : ter{tesSUCCESS};
+            env(credentials::create(alice, alice, "foo"), expected);
+
+            // Destroy all objects in directory
+            for (int i = 0; i < 64; ++i)
+                env(credentials::deleteCred(
+                    alice, alice, alice, std::to_string(i)));
+
+            if (!full)
+                env(credentials::deleteCred(alice, alice, alice, "foo"));
+
+            // Verify directory is empty.
+            auto const sle = env.le(keylet::ownerDir(alice.id()));
+            BEAST_EXPECT(sle == nullptr);
+
+            // Test completed
+            env.close();
+        };
+
+        testCase(
+            testable_amendments() - fixDirectoryLimit,
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory full without fixDirectoryLimit");
+                return {dirNodeMaxPages - 1, true};
+            });
+        testCase(
+            testable_amendments(),  //
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory not full with fixDirectoryLimit");
+                return {dirNodeMaxPages - 1, false};
+            });
+        testCase(
+            testable_amendments(),  //
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory full with fixDirectoryLimit");
+                return {std::numeric_limits<std::uint64_t>::max(), true};
+            });
+    }
+
+    void
     run() override
     {
         testDirectoryOrdering();
@@ -497,6 +584,7 @@ struct Directory_test : public beast::unit_test::suite
         testRipd1353();
         testEmptyChain();
         testPreviousTxnID();
+        testDirectoryFull();
     }
 };
 

src/libxrpl/ledger/ApplyView.cpp
@@ -22,6 +22,9 @@
 #include <xrpl/ledger/ApplyView.h>
 #include <xrpl/protocol/Protocol.h>
 
+#include <limits>
+#include <type_traits>
+
 namespace ripple {
 
 std::optional<std::uint64_t>
@@ -91,8 +94,21 @@ ApplyView::dirAdd(
         return page;
     }
 
+    // We rely on modulo arithmetic of unsigned integers (guaranteed in
+    // [basic.fundamental] paragraph 2) to detect page representation overflow.
+    // For signed integers this would be UB, hence static_assert here.
+    static_assert(std::is_unsigned_v<decltype(page)>);
+    // Defensive check against breaking changes in compiler.
+    static_assert([]<typename T>(std::type_identity<T>) constexpr -> T {
+        T tmp = std::numeric_limits<T>::max();
+        return ++tmp;
+    }(std::type_identity<decltype(page)>{}) == 0);
+    ++page;
     // Check whether we're out of pages.
-    if (++page >= dirNodeMaxPages)
+    if (page == 0)
+        return std::nullopt;
+    if (!rules().enabled(fixDirectoryLimit) &&
+        page >= dirNodeMaxPages)  // Old pages limit
         return std::nullopt;
 
     // We are about to create a new node; we'll link it to

Hugegreencandle/xahaud

include/xrpl/protocol/Protocol.h
@@ -56,7 +56,10 @@ std::size_t constexpr oversizeMetaDataCap = 5200;
 /** The maximum number of entries per directory page */
 std::size_t constexpr dirNodeMaxEntries = 32;
 
-/** The maximum number of pages allowed in a directory */
+/** The maximum number of pages allowed in a directory
+
+    Made obsolete by fixPageCap amendment.
+*/
 std::uint64_t constexpr dirNodeMaxPages = 262144;
 
 /** The maximum number of items in an NFT page */

src/test/app/Credentials_test.cpp
@@ -568,6 +568,39 @@ struct Credentials_test : public beast::unit_test::suite
                     jle[jss::result][jss::node]["CredentialType"] ==
                         strHex(std::string_view(credType)));
             }
+
+            {
+                testcase("Credentials fail, directory full");
+                std::uint32_t const issuerSeq{env.seq(issuer) + 1};
+                env(ticket::create(issuer, 63));
+                env.close();
+
+                // Everything below can only be tested on open ledger.
+                auto const res1 = directory::bumpLastPage(
+                    env,
+                    directory::maximumPageIndex(env),
+                    keylet::ownerDir(issuer.id()),
+                    directory::adjustOwnerNode);
+                BEAST_EXPECT(res1);
+
+                auto const jv = credentials::create(issuer, subject, credType);
+                env(jv, ter(tecDIR_FULL));
+                // Free one directory entry by using a ticket
+                env(noop(issuer), ticket::use(issuerSeq + 40));
+
+                // Fill subject directory
+                env(ticket::create(subject, 63));
+                auto const res2 = directory::bumpLastPage(
+                    env,
+                    directory::maximumPageIndex(env),
+                    keylet::ownerDir(subject.id()),
+                    directory::adjustOwnerNode);
+                BEAST_EXPECT(res2);
+                env(jv, ter(tecDIR_FULL));
+
+                // End test
+                env.close();
+            }
         }
 
         {
@@ -1089,6 +1122,7 @@ struct Credentials_test : public beast::unit_test::suite
         testSuccessful(all);
         testCredentialsDelete(all);
         testCreateFailed(all);
+        testCreateFailed(all - fixPageCap);
         testAcceptFailed(all);
         testDeleteFailed(all);
         testFeatureFailed(all - featureCredentials);

src/test/jtx/directory.h
@@ -1,0 +1,81 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2025 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_TEST_JTX_DIRECTORY_H_INCLUDED
+#define RIPPLE_TEST_JTX_DIRECTORY_H_INCLUDED
+
+#include <test/jtx/Env.h>
+
+#include <xrpl/basics/Expected.h>
+#include <xrpl/protocol/Feature.h>
+#include <xrpl/protocol/Indexes.h>
+
+#include <cstdint>
+#include <limits>
+
+namespace ripple::test::jtx {
+
+/** Directory operations. */
+namespace directory {
+
+enum Error {
+    DirectoryRootNotFound,
+    DirectoryTooSmall,
+    DirectoryPageDuplicate,
+    DirectoryPageNotFound,
+    InvalidLastPage,
+    AdjustmentError
+};
+
+/// Move the position of the last page in the user's directory on open ledger to
+/// newLastPage. Requirements:
+/// - directory must have at least two pages (root and one more)
+/// - adjust should be used to update owner nodes of the objects affected
+/// - newLastPage must be greater than index of the last page in the directory
+///
+/// Use this to test tecDIR_FULL errors in open ledger.
+/// NOTE: effects will be DISCARDED on env.close()
+auto
+bumpLastPage(
+    Env& env,
+    std::uint64_t newLastPage,
+    Keylet directory,
+    std::function<bool(ApplyView&, uint256, std::uint64_t)> adjust)
+    -> Expected<void, Error>;
+
+/// Implementation of adjust for the most common ledger entry, i.e. one where
+/// page index is stored in sfOwnerNode (and only there). Pass this function
+/// to bumpLastPage if the last page of directory has only objects
+/// of this kind (e.g. ticket, DID, offer, deposit preauth, MPToken etc.)
+bool
+adjustOwnerNode(ApplyView& view, uint256 key, std::uint64_t page);
+
+inline auto
+maximumPageIndex(Env const& env) -> std::uint64_t
+{
+    if (env.enabled(fixPageCap))
+        return std::numeric_limits<std::uint64_t>::max();
+    return dirNodeMaxPages - 1;
+}
+
+}  // namespace directory
+
+}  // namespace ripple::test::jtx
+
+#endif

src/test/ledger/Directory_test.cpp
@@ -23,9 +23,11 @@
 #include <xrpl/basics/random.h>
 #include <xrpl/protocol/Feature.h>
 #include <xrpl/protocol/Protocol.h>
+#include <xrpl/protocol/TER.h>
 #include <xrpl/protocol/jss.h>
 
 #include <algorithm>
+#include <limits>
 
 namespace ripple {
 namespace test {
@@ -491,6 +493,93 @@ struct Directory_test : public beast::unit_test::suite
     }
 
     void
+    testDirectoryFull()
+    {
+        using namespace test::jtx;
+        Account alice("alice");
+
+        auto const testCase = [&, this](FeatureBitset features, auto setup) {
+            using namespace test::jtx;
+
+            Env env(*this, features);
+            env.fund(XRP(20000), alice);
+            env.close();
+
+            auto const setupResult = setup(env);
+            auto const lastPage = std::get<0>(setupResult);
+            auto const full = std::get<1>(setupResult);
+
+            // Populate root page and last page
+            for (int i = 0; i < 63; ++i)
+                env(credentials::create(alice, alice, std::to_string(i)));
+            env.close();
+
+            // NOTE, everything below can only be tested on open ledger because
+            // there is no transaction type to express what bumpLastPage does.
+
+            // Bump position of last page from 1 to highest possible
+            auto const res = directory::bumpLastPage(
+                env,
+                lastPage,
+                keylet::ownerDir(alice.id()),
+                [lastPage, this](
+                    ApplyView& view, uint256 key, std::uint64_t page) {
+                    auto sle = view.peek({ltCREDENTIAL, key});
+                    if (!BEAST_EXPECT(sle))
+                        return false;
+
+                    BEAST_EXPECT(page == lastPage);
+                    sle->setFieldU64(sfIssuerNode, page);
+                    // sfSubjectNode is not set in self-issued credentials
+                    view.update(sle);
+                    return true;
+                });
+            BEAST_EXPECT(res);
+
+            // Create one more credential
+            env(credentials::create(alice, alice, std::to_string(63)));
+
+            // Not enough space for another object if full
+            auto const expected = full ? ter{tecDIR_FULL} : ter{tesSUCCESS};
+            env(credentials::create(alice, alice, "foo"), expected);
+
+            // Destroy all objects in directory
+            for (int i = 0; i < 64; ++i)
+                env(credentials::deleteCred(
+                    alice, alice, alice, std::to_string(i)));
+
+            if (!full)
+                env(credentials::deleteCred(alice, alice, alice, "foo"));
+
+            // Verify directory is empty.
+            auto const sle = env.le(keylet::ownerDir(alice.id()));
+            BEAST_EXPECT(sle == nullptr);
+
+            // Test completed
+            env.close();
+        };
+
+        testCase(
+            testable_amendments() - fixPageCap,
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory full without fixPageCap");
+                return {dirNodeMaxPages - 1, true};
+            });
+        testCase(
+            testable_amendments(),  //
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory not full with fixPageCap");
+                return {dirNodeMaxPages - 1, false};
+            });
+        testCase(
+            testable_amendments(),  //
+            [this](Env&) -> std::tuple<std::uint64_t, bool> {
+                testcase("directory full with fixPageCap");
+                return {std::numeric_limits<std::uint64_t>::max(), true};
+            });
+    }
+
+    void
     run() override
     {
         testDirectoryOrdering();
@@ -498,6 +587,7 @@ struct Directory_test : public beast::unit_test::suite
         testRipd1353();
         testEmptyChain();
         testPreviousTxnID();
+        testDirectoryFull();
     }
 };
 

src/xrpld/ledger/detail/ApplyView.cpp
@@ -23,6 +23,9 @@
 #include <xrpl/beast/utility/instrumentation.h>
 #include <xrpl/protocol/Protocol.h>
 
+#include <limits>
+#include <type_traits>
+
 namespace ripple {
 
 std::optional<std::uint64_t>
@@ -92,10 +95,21 @@ ApplyView::dirAdd(
         return page;
     }
 
-    bool const capped = !rules().enabled(fixPageCap);
-
+    // We rely on modulo arithmetic of unsigned integers (guaranteed in
+    // [basic.fundamental] paragraph 2) to detect page representation overflow.
+    // For signed integers this would be UB, hence static_assert here.
+    static_assert(std::is_unsigned_v<decltype(page)>);
+    // Defensive check against breaking changes in compiler.
+    static_assert([]<typename T>(std::type_identity<T>) constexpr -> T {
+        T tmp = std::numeric_limits<T>::max();
+        return ++tmp;
+    }(std::type_identity<decltype(page)>{}) == 0);
+    ++page;
     // Check whether we're out of pages.
-    if (++page >= dirNodeMaxPages && capped)
+    if (page == 0)
+        return std::nullopt;
+    if (!rules().enabled(fixPageCap) &&
+        page >= dirNodeMaxPages)  // Old pages limit
         return std::nullopt;
 
     // We are about to create a new node; we'll link it to